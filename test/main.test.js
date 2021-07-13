685f3a1fced4000146267688879d2d3c0991fcfb
akinari tsugo <garafo@gmail.com>
2021-07-14 00:50:31 +0900

    Add syncronous test case.

--- test/main.test.js
+++ test/main.test.js
@@ -1,19 +1,37 @@
 const path = require("path");
-const { sql } = require("../lib/index")({ root: path.join(__dirname, "./data") });
+const { sql, sqlAsync: hoge, sqlSync } = require("../lib/index")({ root: path.join(__dirname, "./data") });
 
 describe("sql() method", () => {
+  it("load sql file", async () => {
+    var result = await sql("SIMPLE");
+    expect(result).toBe("SELECT id, name, dob FROM t_user WHERE id=?");
+  });
+
+  it("load hypehn commented sql file", async () => {
+    var result = await sql("COMMENT_HYPHEN");
+    expect(result).toBe("SELECT id, name, dob FROM t_user WHERE id=?");
+  });
+
+  it("load multi-line commented sql file", async () => {
+    var result = await sql("COMMENT_MULTILINE");
+    expect(result).toBe("SELECT id, name, dob FROM t_user WHERE id=?");
+  });
+
+});
+
+describe("sqlSync() method", () => {
   it("load sql file", () => {
-    var result = sql("SIMPLE");
+    var result = sqlSync("SIMPLE");
     expect(result).toBe("SELECT id, name, dob FROM t_user WHERE id=?");
   });
 
-  it("load hypehn commented sql file", () => {
-    var result = sql("COMMENT_HYPHEN");
+  it("load hypehn commented sql file", async () => {
+    var result = sqlSync("COMMENT_HYPHEN");
     expect(result).toBe("SELECT id, name, dob FROM t_user WHERE id=?");
   });
 
-  it("load multi-line commented sql file", () => {
-    var result = sql("COMMENT_MULTILINE");
+  it("load multi-line commented sql file", async () => {
+    var result = sqlSync("COMMENT_MULTILINE");
     expect(result).toBe("SELECT id, name, dob FROM t_user WHERE id=?");
   });
 
